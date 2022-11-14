86e0f42dc45fb0cafc27dbdbe11a65c540c9cfc9
James Shuttleworth <digehode@googlemail.com>
2022-11-14 16:09:58 +0000

    Added an intentional logical error

--- src/cards.py
+++ src/cards.py
@@ -13,7 +13,7 @@ def createDeck():
 
     # Suits and numbers to iterate over
     suits=["♥","♠","♦","♣"]
-    numbers=["A","2","3","4","5","6","7","8","9","1","0","J","Q","K"]
+    numbers=["A","2","3","4","5","6","7","8","9","10","11","J","Q","K"]
 
     
     for suit in suits:
