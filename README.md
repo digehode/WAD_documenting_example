692d845bb4fa4941b3866ae8e7acd11243eb9326
James Shuttleworth <digehode@googlemail.com>
2022-11-14 13:38:07 +0000

    Basic content ready for learners

--- README.md
+++ README.md
@@ -5,3 +5,30 @@ Simple program for demonstrating:
  - comments
  - docstrings
  - pdoc
+
+## The `README.md`
+
+This file is also part of documentation of a program. It should
+explain the purpose of the code and give information on how to use it,
+the development environment, etc.
+
+## Generating Documentation
+
+Make sure you have `pdoc` installed (see below) and you're in the root of the
+repository, not in the src directory, unless you want to adjust the
+paths in the commands below.
+
+To view documentation in the terminal, use `pdoc ./src/cards.py`
+
+To create an HTML document, use: `pdoc --force --html ./src/cards.py -o docs/` which will put `cards.html` into the `docs` directory.
+
+There are many more options, which you can read about here: <https://pdoc3.github.io/pdoc/>
+
+## Documenting dependencies
+
+You can create a file, such as the `requirements.txt` file in this
+repository, in which dependencies are listed. You can then also
+install them with `pip install -r requirements.txt`. This means that
+not only do you have a record of requirements, but also a way to
+quickly add them into the virtual environment when starting to work on
+a project,
---
+++ src/cards.py
@@ -0,0 +1,54 @@
+# cards.py
+# Collection of tools for simulating a deck of cards
+import random
+
+def createDeck():
+    """ Create a new standard deck and return it.
+    
+    TODO:
+
+     * Add arguments allowing non-standard decks
+    """
+    deck=[]
+
+    # Suits and numbers to iterate over
+    suits=["♥","♠","♦","♣"]
+    numbers=["A","2","3","4","5","6","7","8","9","1","0","J","Q","K"]
+
+    
+    for suit in suits:
+        for number in numbers:
+            deck.append(number+suit)
+    return deck
+
+
+
+#Note the addition of a type in the argument list below. It is not
+#used by the interpreter, but is good documentation and can be used by
+#source analysis tools.
+
+def shuffle(deck:list):
+    """ Creates and returns a shuffled equivalent of the given deck 
+
+    Arguments:
+
+     * deck: a list of cards
+
+    """
+    newDeck=deck.copy()
+    random.shuffle(newDeck)
+    return newDeck
+
+def deal(deck:list, handSize:int):
+    """ Take the top cards from the deck and return them as a "hand". The deck is modified by this function. 
+
+    Arguments:
+
+     - deck: the deck to draw from
+     - handSize: number of cards to draw into the returned hand
+    """
+    
+    hand=[]
+    while len(hand)<handSize:
+        hand.append(deck.pop(0))
+    return hand
